bf395ceae5fbab8ff78fc5b7d58a3f7d503e471a
at-syot <aotsincere@gmail.com>
2025-01-29 04:47:48 +0700

    refactor keyspair.

--- src/core/cyphers.rs
+++ src/core/cyphers.rs
@@ -1,6 +1,6 @@
 use k256::ecdsa::signature::{Signer, Verifier};
 use k256::ecdsa::Error as EcdsaErr;
-use k256::ecdsa::{Signature, SigningKey, VerifyingKey};
+use k256::ecdsa::{Signature as EcdsaSignature, SigningKey, VerifyingKey};
 use k256::elliptic_curve::rand_core::OsRng;
 use k256::SecretKey;
 
@@ -12,79 +12,81 @@ pub trait Decoder {
     fn decode(&self, encoded: &Vec<u8>) -> Result<Box<Self>, String>;
 }
 
-pub struct KeysPair();
-impl KeysPair {
-    pub fn generate_private() -> String {
-        let private = SecretKey::random(&mut OsRng);
-        hex::encode(private.to_bytes())
+pub struct PrivateKey {
+    key_bytes: Vec<u8>,
+}
+
+impl PrivateKey {
+    pub fn generate() -> Self {
+        let pk = SecretKey::random(&mut OsRng);
+        Self {
+            key_bytes: pk.to_bytes().to_vec(),
+        }
     }
 
-    pub fn derive_public(hex_private_key: &str) -> String {
-        let private_key_bytes = hex::decode(hex_private_key).unwrap();
-        let temp_private = SecretKey::from_slice(&private_key_bytes).unwrap();
-        let public_key = temp_private.public_key().to_sec1_bytes();
-        hex::encode(&public_key)
+    pub fn public_key(&self) -> PublicKey {
+        PublicKey::from(&self)
     }
 
-    pub fn derive_wallet_address(hex_public_key: &str) {
-        let decoded_pub_key = hex::decode(hex_public_key).unwrap();
-        let sha256_hash = sha256::digest(hex_public_key);
-        println!("\ndecoded_pub_key {:?}", decoded_pub_key);
-        println!("sha256_hash {:?}\n", sha256_hash);
-
-        // TODO: derive bitcoin or etheream wallet's address
-    }
-
-    pub fn sign(hex_private_key: &str, data: &[u8]) -> Result<String, EcdsaErr> {
-        let signing_key = hex::decode(hex_private_key)
-            .map_err(|e| EcdsaErr::from_source(e))
-            .and_then(|private_key_bytes| SigningKey::from_slice(&private_key_bytes))
-            .map(|signing_key| signing_key as SigningKey);
-
+    pub fn sign(&self, data: &[u8]) -> Result<Signature, EcdsaErr> {
+        let signing_key = SigningKey::from_slice(&self.as_bytes());
         if let Ok(sk) = signing_key {
-            let signature: Signature = sk.sign(data);
-            let signature_as_hex = hex::encode(signature.to_der().to_bytes());
-            return Ok(signature_as_hex);
+            let signature = sk.sign(data);
+            return Ok(Signature { signature });
         }
 
         Err(signing_key.unwrap_err())
     }
 
-    pub fn verify_signature(
-        hex_pub_key: &str,
-        hex_signature: &str,
-        data: &[u8],
-    ) -> Result<(), EcdsaErr> {
-        let verifying_key = hex::decode(hex_pub_key)
-            .map_err(|e| EcdsaErr::from_source(e))
-            .and_then(|pub_key_bytes| VerifyingKey::from_sec1_bytes(&pub_key_bytes));
+    pub fn as_bytes(&self) -> &Vec<u8> {
+        &self.key_bytes
+    }
+
+    pub fn as_hex(&self) -> String {
+        hex::encode(&self.key_bytes)
+    }
+}
+
+pub struct PublicKey {
+    key_bytes: Vec<u8>,
+}
+
+impl PublicKey {
+    pub fn from(private_key: &PrivateKey) -> Self {
+        let pk_bytes = private_key.as_bytes();
+        let public_key_bytes = SecretKey::from_slice(&pk_bytes)
+            .unwrap() // ignore err
+            .public_key()
+            .to_sec1_bytes()
+            .to_vec();
+
+        Self {
+            key_bytes: public_key_bytes,
+        }
+    }
+
+    pub fn verify(&self, signature: &Signature, data: &[u8]) -> Result<(), EcdsaErr> {
+        let verifying_key = VerifyingKey::from_sec1_bytes(&self.key_bytes);
         if let Err(e) = verifying_key {
             return Err(e);
         }
 
-        let signature = hex::decode(hex_signature)
-            .map_err(|e| EcdsaErr::from_source(e))
-            .and_then(|signature_bytes| Signature::from_der(&signature_bytes));
-
-        if let Err(e) = signature {
-            return Err(e);
-        }
-
-        verifying_key.unwrap().verify(&data, &signature.unwrap())
+        verifying_key.unwrap().verify(&data, &signature.signature)
     }
 }
 
-#[test]
-fn test_keypair() {
-    let hex_private = KeysPair::generate_private();
-    println!("private {hex_private}");
+pub struct Signature {
+    signature: EcdsaSignature,
+}
 
-    let hex_pub = KeysPair::derive_public(&hex_private);
-    println!("public {}", hex_pub);
+impl Signature {
+    pub fn as_bytes(&self) -> Vec<u8> {
+        self.signature.to_der().to_bytes().to_vec()
+    }
 
-    KeysPair::derive_wallet_address(&hex_pub);
-
-    assert!(false)
+    pub fn as_hex(&self) -> String {
+        hex::encode(self.signature.to_der().to_bytes())
+    }
 }
 
 #[test]
@@ -93,11 +95,12 @@ fn sign_block() {
     let b = Block::new("prev_hash".into(), 0, 0, vec![]);
     let encoded_b = &b.encode().unwrap();
 
-    let hex_private = KeysPair::generate_private();
-    let signature = KeysPair::sign(hex_private.as_ref(), &encoded_b).unwrap();
-    assert_eq!(signature.is_empty(), false);
+    let private_key = PrivateKey::generate();
+    // let signature = KeysPair::sign(hex_private.as_ref(), &encoded_b).unwrap();
+    let signature = private_key.sign(&encoded_b).unwrap();
+    assert_eq!(signature.as_bytes().is_empty(), false);
 
-    let hex_public = KeysPair::derive_public(hex_private.as_ref());
-    let result = KeysPair::verify_signature(&hex_public, &signature, &encoded_b);
+    let public_key = private_key.public_key();
+    let result = public_key.verify(&signature, &encoded_b);
     assert!(result.is_ok());
 }
