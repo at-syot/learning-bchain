f4da55bdc3d8e3d605a59fd9b662c3197a7a8d6d
at-syot <aotsincere@gmail.com>
2025-06-04 00:20:42 -0400

    wallet able to create&sign trx, trx serde correctly.

--- src/core/transaction.rs
+++ src/core/transaction.rs
@@ -1,25 +1,50 @@
-use super::cyphers::Encoder;
+use super::cyphers::{Encoder, Signature};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Clone, Deserialize)]
-pub struct Transaction {
+// TODO:
+// - generate wallet address from public key
+// - create & sign transaction [trx-data, trx-signature]
+// - POC: public key recovery from singnature
+
+// creating transaction
+// wallet -> create trx -> send to blockchain -> boardcasting to all nodes
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TransactionData {
     sender_addr: String,
     recipient_addr: String,
     value: f64,
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Transaction {
+    pub data: Vec<u8>,
+    pub signature: Option<Signature>,
+}
+
 impl Transaction {
     pub fn new(sender_addr: String, recipient_addr: String, value: f64) -> Self {
-        Transaction {
+        let trx_data = TransactionData {
             sender_addr,
             recipient_addr,
             value,
+        };
+        let serialized_trx_data = bincode::serialize(&trx_data).unwrap();
+        Transaction {
+            data: serialized_trx_data,
+            signature: None,
         }
     }
 }
 
 impl Encoder for Transaction {
     fn encode(&self) -> Result<Vec<u8>, String> {
-        bincode::serialize(self).map_err(|e| e.to_string())
+        // bincode::serialize(self).map_err(|e| e.to_string())
+        Ok(vec![])
     }
+}
+
+#[test]
+fn trx() {
+    let trx = Transaction::new("a".to_string(), "b".to_string(), 1.0);
 }
--- src/core/wallet.rs
+++ src/core/wallet.rs
@@ -2,6 +2,7 @@ use super::cyphers::{Encoder, PrivateKey, PublicKey, Signature};
 use super::transaction::Transaction;
 use k256::ecdsa::Error as EcdsaErr;
 
+#[derive(Debug)]
 pub struct Wallet {
     data: Vec<u8>,
 
@@ -17,7 +18,7 @@ impl Wallet {
         let public_key = private_key.public_key();
 
         // generate address: !keep simple! -- first 20 of public bytes
-        // address can follow bitcoin or ETH : will be impl
+        // address can follow bitcoin or ETH : will be implemented
         let address = hex::encode(&public_key.as_bytes()[0..20]);
         Ok(Self {
             data,
@@ -31,9 +32,39 @@ impl Wallet {
         Transaction::new(self.address.clone(), recipient_addr, value)
     }
 
-    pub fn sign_transaction(&self, trx: &Transaction) -> Result<Signature, EcdsaErr> {
-        trx.encode()
-            .map_err(|e| EcdsaErr::from_source(e))
-            .and_then(|encoded_trx| self.private_key.sign(&encoded_trx))
+    pub fn sign_transaction(&self, trx: &mut Transaction) -> Result<(), EcdsaErr> {
+        let trx_signature = self.private_key.sign(&trx.data)?;
+        trx.signature = Some(trx_signature);
+        Ok(())
     }
+}
+
+// Test:
+// - wallet successfully sign trx
+// - signed trx can safe & correctly serd to bytes
+// - signed trx is still correct in both before and after serd
+#[test]
+fn wallet() {
+    let w = Wallet::new(vec![]).unwrap();
+    let mut trx = w.create_transaction("recv_hex".to_string(), 1.0);
+    w.sign_transaction(&mut trx);
+    dbg!(&trx.signature);
+
+    // Before:
+    // trx serialize: verify trx data - valid
+    let clone_signature = trx.signature.clone();
+    let valid = w
+        .public_key
+        .verify(&clone_signature.unwrap(), &trx.data[..]);
+    assert!(valid.is_ok());
+
+    // After:
+    // - trx serialize then deserialize back to Transaction struct (is trx's data valid ?)
+    let clone_trx = trx.clone();
+    let ser_trx = bincode::serialize(&clone_trx).expect("ser_trx");
+    let de_trx: Transaction = bincode::deserialize(&ser_trx.as_slice()).expect("de_trx");
+    let after_valid = w
+        .public_key
+        .verify(&de_trx.signature.unwrap(), &de_trx.data);
+    assert!(after_valid.is_ok());
 }
