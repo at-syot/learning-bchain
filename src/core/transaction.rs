2eab451f2a3d37b5a7e2031a8f15d340472784cc
at-syot <aotsincere@gmail.com>
2025-06-13 16:45:49 -0400

    impl TxBuilder pattern to create Tx

--- src/core/transaction.rs
+++ src/core/transaction.rs
@@ -1,43 +1,100 @@
 use super::cyphers::{Encoder, Signature};
+use super::wallet::Wallet;
 use serde::{Deserialize, Serialize};
 
-// Miles stone
-// creating transaction
-// wallet -> create trx -> send to blockchain -> boardcasting to all nodes
+// Transaction struct
+// - Version no. Flag
+// - In-counter: total number of inputs
+// - Inputs: List of inputs
+// - Out-counter:
+// - Outputs:  List of outputs
+// - LockTime
+// - TxId: Hash (e.g., 256-bit hash) - The unique identifier of this entire transaction.
+//         It's typically the cryptographic hash of the entire transaction's data structure (excluding the TxId itself).
+//         This is calculated *after* the transaction is formed.
 
-// TODO:
-// - generate wallet address from public key: DONE
-// - create & sign transaction [trx-data, trx-signature]: DONE
-// - POC: public key recovery from singnature: DONE
-//
-// DOING
-// - blockchain network
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TransactionData {
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct TransactionData {
     sender_addr: String,
-    recipient_addr: String,
+    receiver_addr: String,
     value: f64,
+
+    inputs: Vec<Transaction>,
+    outputs: Vec<Transaction>,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Transaction {
+    pub trx_id: String,
     pub data: Vec<u8>,
-    pub signature: Option<Signature>,
+    pub signature: Signature,
 }
 
-impl Transaction {
-    pub fn new(sender_addr: String, recipient_addr: String, value: f64) -> Self {
-        let trx_data = TransactionData {
+pub enum TxBuilderErr {
+    RequiredInputs,
+    RequiredOutputs,
+    SerializeFail,
+    SignTxFail,
+}
+pub type TxBuilderResult = Result<Transaction, TxBuilderErr>;
+
+pub struct TxBuilder {
+    sender_addr: String,
+    receiver_addr: String,
+    value: f64,
+    inputs: Option<Vec<Transaction>>,
+    outputs: Option<Vec<Transaction>>,
+    tx_id: Option<String>,
+}
+
+impl TxBuilder {
+    pub fn new(sender_addr: String, receiver_addr: String, value: f64) -> Self {
+        Self {
             sender_addr,
-            recipient_addr,
+            receiver_addr,
             value,
-        };
-        let serialized_trx_data = bincode::serialize(&trx_data).unwrap();
-        Transaction {
-            data: serialized_trx_data,
-            signature: None,
+            inputs: None,
+            outputs: None,
+            tx_id: None,
         }
+    }
+
+    pub fn inputs(mut self, inputs: Vec<Transaction>) -> Self {
+        self.inputs = Some(inputs);
+        self
+    }
+
+    pub fn outputs(mut self, outputs: Vec<Transaction>) -> Self {
+        self.outputs = Some(outputs);
+        self
+    }
+
+    pub fn sign(self, wallet: &Wallet) -> Result<Transaction, TxBuilderErr> {
+        if self.inputs.is_none() {
+            return Err(TxBuilderErr::RequiredInputs);
+        }
+        if self.outputs.is_none() {
+            return Err(TxBuilderErr::RequiredOutputs);
+        }
+
+        let tx_data = TransactionData {
+            sender_addr: self.sender_addr,
+            receiver_addr: self.receiver_addr,
+            value: self.value,
+            inputs: self.inputs.unwrap(),
+            outputs: self.outputs.unwrap(),
+        };
+        let encoded_tx_data =
+            bincode::serialize(&tx_data).map_err(|_| TxBuilderErr::SerializeFail)?;
+        let tx_sig = wallet
+            .sign_data(encoded_tx_data.as_slice())
+            .map_err(|_| TxBuilderErr::SignTxFail)?;
+
+        Ok(Transaction {
+            trx_id: String::new(),
+            data: encoded_tx_data,
+            signature: tx_sig,
+        })
     }
 }
 
@@ -46,9 +103,4 @@ impl Encoder for Transaction {
         // bincode::serialize(self).map_err(|e| e.to_string())
         Ok(vec![])
     }
-}
-
-#[test]
-fn trx() {
-    let trx = Transaction::new("a".to_string(), "b".to_string(), 1.0);
 }
